60c87551478028acddf2264ce460c886aa5fc4cd
Jorch <jorch.rl@gmail.com>
2021-12-22 23:28:18 -0600

    Add template.js html generator

---
+++ template.js
@@ -0,0 +1,14 @@
+export default () => {
+  return `<!doctype html>
+  <html lang=en>
+    <head>
+      <meta charset="utf-8">
+      <title>MERN Stack App</title>
+    </head>
+    <body>
+      <div id="root">
+        <h2>Hello World :3</h2>
+      </div>
+    </body>
+  </html>`;
+};
