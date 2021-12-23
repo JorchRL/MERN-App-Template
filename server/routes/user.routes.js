3ca95265c729cd7d1c3bcc164746ef09ce9ba739
Jorch <jorch.rl@gmail.com>
2021-12-23 11:51:42 -0600

    Setup controller callbacks for the user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
+import userCtrl from "../controllers/user.controller";
 
 const router = express.Router();
 
-router.route("/api/users").get().post();
+router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
 
-router.route("/api/users/:userId").get().put().delete();
+router
+  .route("/api/users/:userId")
+  .get(userCtrl.read)
+  .put(userCtrl.update)
+  .delete(userCtrl.remove);
+
+router.param("userId", userCtrl.userByID);
 
 export default route;
