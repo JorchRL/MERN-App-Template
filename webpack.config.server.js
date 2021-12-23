45ee254d4434b26c5696bcfb58106e8dc6f575d2
Jorch <jorch.rl@gmail.com>
2021-12-22 23:27:48 -0600

    Add webpack rules for file-loader module

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
+// const webpack = require("webpack");
 const CURRENT_WORKING_DIR = process.cwd();
 const nodeExternals = require("webpack-node-externals");
 
@@ -20,6 +20,10 @@ const config = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
+      },
+      {
+        test: /\.(ttf|eot|svg|jpg|png)(\?[\s\S]+)?$/,
+        use: "file-loader",
       },
     ],
   },
