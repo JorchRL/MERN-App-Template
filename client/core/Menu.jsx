a8028f8c903e3af41e582e82901fe89d32e5cd90
Jorch <jorch.rl@gmail.com>
2022-01-15 19:37:54 -0600

    Re-do Menu component

--- client/core/Menu.jsx
+++ client/core/Menu.jsx
@@ -1,32 +1,83 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { IconButton, makeStyles, Button } from "@material-ui/core";
 
 import { AppBar } from "@material-ui/core";
 import { Toolbar } from "@material-ui/core";
-import { HomeIcon } from "@material-ui/icons";
+import { Home, HomeIcon } from "@material-ui/icons";
 import { Typography } from "@material-ui/core";
-import auth from "./../auth/auth-helper";
 
-import { Link, withRouter } from "react-router-dom";
+import { auth } from "./../auth/auth-helper";
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const isActive = (location, path) => {
+  if (location.pathname == path) {
+    return { color: "#ff4081" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
 
 const Menu = () => {
+  // I will use hooks instead of withRouter HOC
+  // history.location used to return the same object as the useLocation() hook
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const classes = useStyles();
+
+  const signOut = () => {
+    auth.clearJWT(() => navigate("/"));
+  };
+
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography>MERN App</Typography>
-        <Link className={classes.link} to='/users'>
-          Users
+        <Typography variant='h6' color='inherit'>
+          KatoBook
+        </Typography>
+
+        <Link to='/'>
+          <IconButton aria-label='Home' style={isActive(location, "/")}>
+            <Home />
+          </IconButton>
         </Link>
-        <Link className={classes.link} to='/signup'>
-          Sign Up
+
+        <Link to='/users'>
+          <Button style={isActive(location, "/users")}>Users</Button>
         </Link>
-        <Link className={classes.link} to='/signin'>
-          Sign In
-        </Link>
-        <Link className={classes.link} to='/user/1'>
-          Profile-Example
-        </Link>
+        {
+          // Menu when no user is logged in
+          !auth.isAuthenticated() && (
+            <span>
+              <Link to='/signup'>
+                <Button style={isActive(location, "/signup")}>Sign Up</Button>
+              </Link>
+              <Link to='/signin'>
+                <Button style={isActive(location, "/signin")}>Sign In</Button>
+              </Link>
+            </span>
+          )
+        }
+        {
+          // Menu when the user is logged in
+          auth.isAuthenticated() && (
+            <span>
+              <Link to={`/user/${auth.isAuthenticated().user._id}`}>
+                <Button
+                  style={isActive(
+                    location,
+                    `/user/${auth.isAuthenticated().user._id}`
+                  )}>
+                  My Profile
+                </Button>
+              </Link>
+              <Button color='inherit' onClick={signOut}>
+                Sign Out
+              </Button>
+            </span>
+          )
+        }
       </Toolbar>
     </AppBar>
   );
